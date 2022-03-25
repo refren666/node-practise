eb1307e9d409bdbb5b9b6829019bf3e238de447f
Denys Shvaiber <91616072+refren666@users.noreply.github.com>
2022-03-25 17:41:33 +0200

    Delete lesson_7_jwt/dist directory

--- lesson_7_jwt/dist/routes/postRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const postController = require('../controllers/postController');
-const postRouter = (0, express_1.Router)();
-postRouter.get('/:userId', postController.getPostsByUserId);
-postRouter.patch('/:userId', postController.updatePostTextById);
-module.exports = postRouter;
-//# sourceMappingURL=postRouter.js.map
